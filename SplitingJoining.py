37e29f1d73e401ef3cf1b75182e6cfc4bada8dfb
sohan110111 <sohan110111@gmail.com>
2020-04-02 22:32:52 +0600

    code with Rafeh Qazi clever programming

---
+++ SplitingJoining.py
@@ -0,0 +1,6 @@
+problems = 'broke, pale, short, nerdy'
+print(problems)
+
+li = problems.split(", ")
+print(li)
+print(problems.split('short'))
--- advancedIndexing.py
+++ advancedIndexing.py
@@ -14,12 +14,28 @@ print(digits[:3]) # output = [0, 1, 2]; here show the parts of the array
 print(digits[2:4]) # output = [2, 3]; here 2 is inclusive and 4 is exclusive
 print(digits[0:9]) # output = [0, 1, 2, 3, 4, 5, 6, 7, 8]
 print(digits[0:-1]) # output = [0, 1, 2, 3, 4, 5, 6, 7, 8]
-print(digits[0:10:3]) # output = ; slicing element
-print(digits[::]) # output = ; here show output everything
-print(digits[::1]) # output = ; here show output everything
-print(digits[::2]) # output = ; here show element first index and also show third element expect to first element.
-print(digits[::-1]) # output = ; It go from the end of the list
-print(digits[::-2])  #output = ;
-print(digits[::-3])  #output = ;
-name = ["first last"]
-print(name[:5]) # output = ; here output first 
+print(digits[0:10:3]) # output = [0, 3, 6, 9]; slicing element
+print(digits[::]) # output = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]; here show output everything
+print(digits[::1]) # output = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]; here show output everything
+print(digits[::2]) # output = [0, 2, 4, 6, 8]; here show element first index and also show third element expect to first element.
+print(digits[::-1]) # output = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]; It go from the end of the list (backward=decending of the list of the element)
+print(digits[::-2])  #output = [9, 7, 5, 3, 1];
+print(digits[::-3])  #output = [9, 6, 3, 0];
+print(digits[0:5:-2]) #output = [5, 4, 3, 2, 0]
+for i in range(len(digits)) :
+    print(digits[0:i])
+for i in range(len(digits)) :
+    print(digits[i:i+3])
+for i in range(len(digits)-2) :
+    print(digits[i:i+3])
+window_size = 3
+for i in range(len(digits)-2) :
+    print(digits[i:i+window_size])
+window_size1 = 7
+for i in range(len(digits)-(window_size1-1)) : #Such math notation: 10 - (5 - 1) = 6; 10 - 5 - 1 = 4
+    print(digits[i:i+window_size1])
+window_size2 = 1
+for i in range(len(digits)-(window_size2-1)) :
+    print(digits[i:i+window_size2])
+name = "first last"
+print(name[0:5]) # output = first ;
