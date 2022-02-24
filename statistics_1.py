2b6fce2b196ffd658a727a7b9cfba2d537e65532
Md. Shafiul Islam Sohan <sohan110111@gmail.com>
2022-02-24 11:22:03 +0600

    Statistics module part_1

---
+++ statistics_1.py
@@ -0,0 +1,7 @@
+from re import X
+import statistics
+x = [8, 12, 14, 16, 9, 21, 16]
+print(statistics.mean(x))
+print(statistics.median(x))
+print(statistics.mode(x))
+print(statistics.harmonic_mean(x))
